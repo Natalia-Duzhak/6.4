2cf965d28d7956f0ca6f618c9f623807f178409a
Natalia-Duzhak <72571464+Natalia-Duzhak@users.noreply.github.com>
2021-05-11 15:26:42 +0300

    Add files via upload

---
+++ UnitTest6.4/UnitTest6.4.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project6.4/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest64
+{
+	TEST_CLASS(UnitTest64)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+			matrix<int> A(0,0), B(1,1),C(1,1);
+			
+			Assert::AreEqual(A[11], -3634177);
+		}
+	};
+}
